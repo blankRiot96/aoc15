9116d4af30fa785cfefa8649784ae8156f1ec5fa
Axis <blankRiot96@gmail.com>
2025-07-09 15:32:31 +0530

    day5 p2 midway

--- src/day5.rs
+++ src/day5.rs
@@ -35,3 +35,45 @@ pub fn part_1() {
 
     println!("{n_nice_strings}");
 }
+
+pub fn part_2() {
+    let input = include_str!("inputs/day5.txt");
+    // let input = "qjhvhtzxzqqjkmpb";
+    let mut n_nice_strings = 0;
+
+    for line in input.lines() {
+        let chars: Vec<char> = line.chars().collect();
+        let mut condition1 = false;
+        let mut condition2 = false;
+
+        for (index, c) in chars.iter().enumerate() {
+            if index < 2 {
+                continue;
+            }
+
+            if &chars[index - 2] == c {
+                condition2 = true;
+            }
+        }
+
+        let mut vec2 = chars.clone();
+        vec2.remove(0);
+
+        let pairs: Vec<(&char, char)> = chars.iter().zip(vec2).collect();
+
+        for (index1, pair1) in pairs.iter().enumerate() {
+            for (index2, pair2) in pairs.iter().enumerate() {
+                if pair1 == pair2 && index1 != index2 {
+                    condition1 = true;
+                }
+            }
+        }
+
+        // println!("{condition1} {condition2}");
+        if condition1 && condition2 {
+            n_nice_strings += 1;
+        }
+    }
+
+    println!("{n_nice_strings}");
+}
--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,5 @@ mod day3;
 mod day4;
 mod day5;
 fn main() {
-    day5::part_1();
+    day5::part_2();
 }
