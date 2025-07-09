3fe493021cfabf93fd7de554d261f8157a78f280
Axis <blankRiot96@gmail.com>
2025-07-10 01:09:13 +0530

    day7 p1 completed (slightly inspired)

---
+++ src/day7.rs
@@ -0,0 +1,48 @@
+use std::collections::HashMap;
+
+pub fn part_1() {
+    let input = include_str!("inputs/day7.txt");
+    let mut instructions: HashMap<&str, &str> = HashMap::new();
+    for line in input.lines() {
+        let (expr, dest) = line.split_once(" -> ").unwrap();
+        instructions.insert(dest, expr);
+    }
+    let mut variables: HashMap<&str, u16> = HashMap::new();
+
+    fn eval<'a>(
+        wire: &'a str,
+        instructions: &HashMap<&'a str, &'a str>,
+        variables: &mut HashMap<&'a str, u16>,
+    ) -> u16 {
+        if let Ok(n) = wire.parse::<u16>() {
+            return n;
+        }
+
+        if let Some(var) = variables.get(wire) {
+            return *var;
+        }
+
+        let expr = instructions.get(wire).unwrap();
+        let tokens: Vec<&str> = expr.split_whitespace().collect();
+
+        let value = match tokens.as_slice() {
+            [x] => eval(x, instructions, variables),
+            ["NOT", x] => !eval(x, instructions, variables),
+            [x, "AND", y] => eval(x, instructions, variables) & eval(y, instructions, variables),
+            [x, "OR", y] => eval(x, instructions, variables) | eval(y, instructions, variables),
+            [x, "LSHIFT", y] => {
+                eval(x, instructions, variables) << eval(y, instructions, variables)
+            }
+            [x, "RSHIFT", y] => {
+                eval(x, instructions, variables) >> eval(y, instructions, variables)
+            }
+            _ => panic!("err"),
+        };
+
+        variables.insert(wire, value);
+        value
+    }
+
+    let value = eval("a", &instructions, &mut variables);
+    println!("{value}");
+}
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod day3;
 mod day4;
 mod day5;
 mod day6;
+mod day7;
 fn main() {
-    day6::part_2();
+    day7::part_1();
 }
