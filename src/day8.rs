e6180e42d7df1768e5f5b5e2d253d0b4846fc5b8
Axis <blankRiot96@gmail.com>
2025-07-10 15:31:09 +0530

    day8 p1 solved

--- src/day8.rs
+++ src/day8.rs
@@ -1,22 +1,22 @@
 pub fn part_1() {
-    fn small_parser<'t>(line: &'t str) -> String {
-        let mut result = String::new();
+    fn small_parser<'t>(line: &'t str) -> Vec<u8> {
+        let mut result: Vec<u8> = Vec::new();
         let mut chars = line[1..line.len() - 1].chars().peekable();
 
         while let Some(c) = chars.next() {
             match c {
                 '\\' => match chars.next() {
-                    Some('\\') => result.push('\\'),
-                    Some('"') => result.push('"'),
+                    Some('\\') => result.push('\\' as u8),
+                    Some('"') => result.push('"' as u8),
                     Some('x') => {
                         let h1 = chars.next().unwrap();
                         let h2 = chars.next().unwrap();
                         let byte = u8::from_str_radix(&format!("{h1}{h2}"), 16).unwrap();
-                        result.push(byte as char);
+                        result.push(byte);
                     }
                     _ => panic!("Unrecognized escape sequence :3"),
                 },
-                _ => result.push(c),
+                _ => result.push(c as u8),
             }
         }
 
@@ -32,7 +32,7 @@ pub fn part_1() {
         memory_chars += small_parser(&line).len();
     }
 
-    println!("code_chars = {code_chars}");
-    println!("memory_chars = {memory_chars}");
+    // println!("code_chars = {code_chars}");
+    // println!("memory_chars = {memory_chars}");
     println!("{}", code_chars - memory_chars);
 }
